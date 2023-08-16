f2d8dc4d3ab7b267c813bee12e33de8335b8a3e1
Ivan <ivanulanovskij143@gmail.com>
2023-08-16 16:22:01 +0500

    написана документация

--- create_model.py
+++ create_model.py
@@ -1,6 +1,33 @@
 from keras import Sequential, Model, layers
 from keras.optimizers import Adam
 
+## \brief Функция для создания модели
+## \authors ivan-dev-lab
+## \version 1.0.0
+## \date 16.08.2023
+## \param[in] input_shape Размерность входных данных. Необходима для входного слоя модели
+## \details Ниже представлен исходный код архитектуры модели
+## \code
+# model = Sequential()
+# model.add(layers.Dense(units=256, activation="relu", input_shape=(input_shape,)))
+# model.add(layers.Dropout(0.4))
+# model.add(layers.Dense(units=128, activation="relu"))
+# model.add(layers.Dropout(0.3))
+# model.add(layers.Dense(units=64, activation="relu"))
+# model.add(layers.Dense(units=32, activation="relu"))
+# model.add(layers.Dense(units=1, activation="relu"))
+## \endcode
+## \details Модель скомпилирована с использованием таких функций, как:
+## <ol>
+## <li>Функция-оптимизатор: <b>Adam</b> с фактором обучения: <b>0.001</b></li>
+## <li>Функция потерь: <b>RMSprop</b> с фактором обучения: <b>0.01</b></li>
+## <li>Метрики: <b>[ MeanSquaredError() ]</b></li>
+## </ol>
+## \details Ниже представлен исходный код компиляции модели
+## \code
+# model.compile(optimizer=RMSprop(learning_rate= 0.01), loss="mse", metrics=[MeanSquaredError()])
+## \endcode
+## \return keras.Model - скомпилированная, но не обученная модель
 def create_model (input_shape: int) -> Model:
     model = Sequential()
     model.add(layers.Dense(units=256, activation="relu", input_shape=(input_shape,)))
--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,18 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
+## \brief Функция предобработки данных
+## \authors ivan-dev-lab
+## \version 1.0.0
+## \date 16.08.2023
+## \param[in] filepath путь до необработанных данных 
+## \details Т.к столбцы Gender, Subscription Type и Contract Length содержат категориальные значение, то было принято решение использовать pd.get_dummies для этиъ столбцов
+## \code
+# data = pd.get_dummies(data, prefix=["gender"], columns=["Gender"], dtype=int)
+# data = pd.get_dummies(data, prefix=["sub_type"], columns=["Subscription Type"], dtype=int)
+# data = pd.get_dummies(data, prefix=["contract_len"], columns=["Contract Length"], dtype=int)
+## \endcode
+## \return Кортеж tuple(), содержащий признаки и целевые переменные
 def preprocess (filepath: str) -> tuple:
     data = pd.read_csv(filepath)
 
