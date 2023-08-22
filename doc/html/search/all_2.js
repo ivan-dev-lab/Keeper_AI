6fd02bd31e16bac0ecbf8f30bcfb20fb08f4bcb9
Ivan <ivanulanovskij143@gmail.com>
2023-08-22 20:15:03 +0500

    написана документация по файлу main.py

--- doc/html/search/all_2.js
+++ doc/html/search/all_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['main_0',['main',['../namespacemain.html',1,'main'],['../namespacemain.html#a3140e9a5b6a71ffbf498198cfc471b88',1,'main.main()']]],
-  ['main_2epy_1',['main.py',['../main_8py.html',1,'']]]
+  ['main_2epy_1',['main.py',['../main_8py.html',1,'']]],
+  ['make_5fcommunication_2',['make_communication',['../namespacemain.html#a84b46898f03320e2277e5a992264f133',1,'main']]]
 ];
--- doc/html/search/functions_2.js
+++ doc/html/search/functions_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['main_0',['main',['../namespacemain.html#a3140e9a5b6a71ffbf498198cfc471b88',1,'main']]]
+  ['main_0',['main',['../namespacemain.html#a3140e9a5b6a71ffbf498198cfc471b88',1,'main']]],
+  ['make_5fcommunication_1',['make_communication',['../namespacemain.html#a84b46898f03320e2277e5a992264f133',1,'main']]]
 ];
