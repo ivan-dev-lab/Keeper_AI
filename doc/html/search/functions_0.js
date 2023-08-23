d0647b5eb66b16c87264506748f9e714cbbff12e
Ivan <ivanulanovskij143@gmail.com>
2023-08-23 09:12:45 +0500

    файлы документации

--- doc/html/search/functions_0.js
+++ doc/html/search/functions_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['check_5fboth_5fargs_0',['check_both_args',['../namespacemain.html#a43a6854ed25f8c56635dbdb0eb8bf507',1,'main']]],
+  ['check_5fboth_5fargs_0',['check_both_args',['../namespacemain.html#a874883206ca94dcaa0581c4534c40343',1,'main']]],
   ['create_5fmodel_1',['create_model',['../namespacecreate__model.html#ab0b0a2f67b218e74ff299799e3b465ff',1,'create_model']]],
   ['create_5fmodels_5fcharts_2',['create_models_charts',['../namespacerate.html#a3bdb43f1a48f13278be89f28a3643397',1,'rate']]]
 ];
