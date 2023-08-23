69e4ece08ea8cd7a8252e117cef2d30ba63d9fd6
Ivan <ivanulanovskij143@gmail.com>
2023-08-23 20:05:10 +0500

    документация к проекту

--- doc/html/namespacemain.js
+++ doc/html/namespacemain.js
@@ -1,6 +1,7 @@
 var namespacemain =
 [
     [ "IncorrectNumberOfArgumentsError", "classmain_1_1_incorrect_number_of_arguments_error.html", null ],
+    [ "create_request", "namespacemain.html#a37199c4ba52ad4ba646dc10e1d31cdaf", null ],
     [ "main", "namespacemain.html#a3140e9a5b6a71ffbf498198cfc471b88", null ],
     [ "make_communication", "namespacemain.html#a84b46898f03320e2277e5a992264f133", null ]
 ];
