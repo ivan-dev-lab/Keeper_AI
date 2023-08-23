80e6e4a507f58cd334ca41a07df6f940c21371de
Ivan <ivanulanovskij143@gmail.com>
2023-08-23 12:06:53 +0500

    подправлены функции make_communication, main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 ## \brief Пользовательское исключение
 ## \authors ivan-dev-lab-home
@@ -13,27 +14,28 @@ class IncorrectNumberOfArgumentsError (Exception): pass
 
 ## \brief Функция-коммуникатор между пользователем и моделью
 ## \authors ivan-dev-lab-home
-## \version 1.1.1
+## \version 1.1.2
 ## \date 22.08.2023
 ## \details Функция обеспечивает коммуникацию между моделью и пользователем путем создания флагов для комадной строки
 ## \returns Пространство имен argparse.Namespace
 def make_communication () -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Keeper_AI - Cистема классификации клиентов.")
 
-    parser.add_argument("--clients", type=str, help="путь до исходных данных в формате [.CSV|.XLSX]")
-    parser.add_argument("--pred", type=str, help="предолагаемый путь до конечных данных в формате [.CSV|.XLSX]")
+    data_arg_group = parser.add_argument_group(title="Сохранение и загрузка данных", description="Если не указывать флаг --pred, то данные с предсказаниями будут загружаться в C:/Users/User/Keeper_AI-work/predictions/clients.csv")
+    data_arg_group.add_argument("--clients", type=str, help="Путь до исходных данных в формате [.CSV|.XLSX]")
+    data_arg_group.add_argument("--pred", type=str, help="Путь до предполагаемого файла в формате [.CSV|.XLSX] с конечными данными", default="C:/Users/User/Keeper_AI-work/predictions/clients.csv")
     
-    rate_arg_group = parser.add_argument_group(title="Оценка различных моделей", description="При использовании только флага --rate из данной группы будут построены ТОЛЬКО графики, которые будут сохранены по умолчанию в C:/Users/User/Keeper_AI-work/rating/")
+    rate_arg_group = parser.add_argument_group(title="Оценка различных моделей", description="При использовании только флага --rate из данной группы все будет сохранено в каталог по умолчанию в C:/Users/User/Keeper_AI-work/rating/ . Если не использовать свое значение при флаге --num_top, то топ будет включать в себя 3 позиции")
     rate_arg_group.add_argument("--rate", action="store_true", help="флаг определяет выполнение задачи оценки по умолчанию")
-    rate_arg_group.add_argument("--charts", type=str, help="Путь до предполагаемой папки с графиками")
-    rate_arg_group.add_argument("--best", type=str, help="Путь до предполагаемого файла .CSV с оценкой моделей")
+    rate_arg_group.add_argument("--charts", type=str, help="Путь до предполагаемой папки с графиками", default="C:/Users/User/Keeper_AI-work/rating/charts/")
+    rate_arg_group.add_argument("--best", type=str, help="Путь до предполагаемого файла в формате [.CSV|.XLSX] с оценкой моделей", default="C:/Users/User/Keeper_AI-work/rating/best.csv")
+    rate_arg_group.add_argument("--top", type=str, help="Путь до предполагаемого файла в формате [.CSV|.XLSX] с топ N моделей", default="C:/Users/User/Keeper_AI-work/rating/top.csv")
     rate_arg_group.add_argument("--num_top", type=int, help="Длина составляемого топа", default=3)
-    rate_arg_group.add_argument("--top", type=str, help="Путь до предполагаемого файла .CSV с топ 3 моделей")
 
-    parser.add_argument("--train", action="store_true", help="флаг определяет необходимость обучения моделей")
+    train_arg_group = parser.add_argument_group(title="Тренировка моделей", description="При тренировки модели для предсказаний и моделей для оценки, будут использоваться данные по-умолчанию из каталога Keeper_AI/data/train.csv")
+    train_arg_group.add_argument("--train", action="store_true", help="Флаг определяет необходимость обучения моделей")
     
     return parser.parse_args()
-
 
 ## \brief Главная функция в которой собраны все остальные функци проекта
 ## \authors ivan-dev-lab-home
@@ -46,21 +48,21 @@ def main ():
 
     for arg in args._get_kwargs():
         #print(index,arg)
-        if arg[0] == "clients":
+        if arg[0] == "clients" and os.path.exists(arg[1]):
             request["path_clients"] = arg[1]
         elif arg[0] == "train":
             request["need_train"] = arg[1]
-        if arg[0] == "pred":
+        if arg[0] == "pred" and os.path.exists(arg[1]):
             request["path_pred"] = arg[1]
         elif arg[0] == "rate":
             request["need_rate"] = arg[1]
-        elif arg[0] == "charts":
+        elif arg[0] == "charts" and os.path.exists(arg[1]):
             request["path_charts"] = arg[1]
-        elif arg[0] == "best":
-            request["path_best"] = arg[1]
+        elif arg[0] == "best" and os.path.exists(arg[1]):
+            request["path_best"]  = arg[1]
         elif arg[0] == "num_top":
             request["num_top"] = arg[1]
-        elif arg[0] == "top":
+        elif arg[0] == "top" and os.path.exists(arg[1]):
             request["path_top"] = arg[1]
             
     for name,value in request.items():
