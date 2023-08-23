4934e503484946c57a654283f32222ea457ff42f
Ivan <ivanulanovskij143@gmail.com>
2023-08-23 11:45:59 +0500

    файлы документации

--- doc/html/main_8py.js
+++ doc/html/main_8py.js
@@ -1,7 +1,6 @@
 var main_8py =
 [
     [ "main.IncorrectNumberOfArgumentsError", "classmain_1_1_incorrect_number_of_arguments_error.html", null ],
-    [ "check_both_args", "main_8py.html#a874883206ca94dcaa0581c4534c40343", null ],
     [ "main", "main_8py.html#a3140e9a5b6a71ffbf498198cfc471b88", null ],
     [ "make_communication", "main_8py.html#a84b46898f03320e2277e5a992264f133", null ]
 ];
--- doc/html/namespacemain.js
+++ doc/html/namespacemain.js
@@ -1,7 +1,6 @@
 var namespacemain =
 [
     [ "IncorrectNumberOfArgumentsError", "classmain_1_1_incorrect_number_of_arguments_error.html", null ],
-    [ "check_both_args", "namespacemain.html#a874883206ca94dcaa0581c4534c40343", null ],
     [ "main", "namespacemain.html#a3140e9a5b6a71ffbf498198cfc471b88", null ],
     [ "make_communication", "namespacemain.html#a84b46898f03320e2277e5a992264f133", null ]
 ];
