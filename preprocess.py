b5c77ecd69516a67f97403cbdc8d8493b408f80c
Ivan <ivanulanovskij143@gmail.com>
2023-08-24 23:33:01 +0500

    убрана полоса загрузки

--- preprocess.py
+++ preprocess.py
@@ -1,11 +1,10 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from progress.bar import IncrementalBar
 
 ## \brief Функция предобработки данных
 ## \authors ivan-dev-lab
-## \version 1.0.1
-## \date 23.08.2023
+## \version 1.0.2
+## \date 24.08.2023
 ## \param[in] filepath путь до необработанных данных 
 ## \param[in] data_type Определение типа данных для функции. От параметра зависит - будет ли функция возвращать целевую переменную ( data_type="train" ) или только признаки ( data_type="test" )
 ## \details Т.к столбцы Gender, Subscription Type и Contract Length содержат категориальные значение, то было принято решение использовать pd.get_dummies для этиъ столбцов
@@ -16,8 +15,6 @@ from progress.bar import IncrementalBar
 ## \endcode
 ## \return Кортеж tuple(), содержащий признаки и целевые переменные
 def preprocess (filepath: str, data_type: str) -> tuple:
-    bar = IncrementalBar('Предобработка данных...', max=6)
-
     if data_type == "train":
         data = pd.read_csv(filepath)
     else:
@@ -25,31 +22,25 @@ def preprocess (filepath: str, data_type: str) -> tuple:
 
     data.drop(columns=["CustomerID", "Tenure", "Last Interaction"], inplace=True)
     data.dropna(inplace=True)
-    bar.next()
 
     data = pd.get_dummies(data, prefix=["gender"], columns=["Gender"], dtype=int)
-    bar.next()
     data = pd.get_dummies(data, prefix=["sub_type"], columns=["Subscription Type"], dtype=int)
-    bar.next()
     data = pd.get_dummies(data, prefix=["contract_len"], columns=["Contract Length"], dtype=int)
-    bar.next()
 
     if data_type == "train":
         X = data.drop(columns=["Churn"])
         Y = data["Churn"]
         X_columns = X.columns
-        bar.next()
 
         scaler = StandardScaler().fit(X)
         X = pd.DataFrame(data=scaler.transform(X), columns=X_columns)
-        bar.next()
+
         return (X, Y)
     else:
         X = data.copy()
         X_columns = X.columns
-        bar.next()
 
         scaler = StandardScaler().fit(X)
         X = pd.DataFrame(data=scaler.transform(X), columns=X_columns)
-        bar.next()
+        
         return X
