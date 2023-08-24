53eddcdf84101d3d10d8943c761fb75aa9ef8caf
Ivan <ivanulanovskij143@gmail.com>
2023-08-24 17:53:21 +0500

    добавлены полоса загрузки и документация

--- rate.py
+++ rate.py
@@ -7,6 +7,7 @@ from sklearn.ensemble import HistGradientBoostingClassifier, ExtraTreesClassifie
 from sklearn.tree import DecisionTreeClassifier
 import joblib
 import os
+from progress.bar import IncrementalBar
 from preprocess import preprocess
 from create_model import create_model
 
@@ -30,7 +31,7 @@ Y = response_tuple[1]
 
 ## \brief Функция-оценщик моделей
 ## \authors ivan-dev-lab
-## \version 1.1.0
+## \version 1.1.1
 ## \date 05.08.2023
 ## \param[in] X Признаки входных данных 
 ## \param[in] Y Целевые переменные входных данных 
@@ -40,7 +41,7 @@ Y = response_tuple[1]
 # joblib.dump(model, f"models/{name}.pkl")
 ## \endcode
 ## \code
-# joblib.dump(model, f"models/MyModelRegression.pkl")
+# joblib.dump(model, f"models/ModelRegression.pkl")
 ## \endcode
 ## \return Кортеж tuple(), содержащий названия моделей и результаты их обучения ( mse, mae, r2_score )
 def rate_models (X: pd.DataFrame, Y: pd.DataFrame, verbose=True) -> tuple:
@@ -55,10 +56,13 @@ def rate_models (X: pd.DataFrame, Y: pd.DataFrame, verbose=True) -> tuple:
         'GradientBoostingClassifier': GradientBoostingClassifier,
         'DecisionTreeClassifier': DecisionTreeClassifier
     }
+
+    bar = IncrementalBar('Оценка моделей...', max=len(models)+4)
     
     names, mse_scores, mae_scores, r2_scores = [], [], [], []
 
     x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    bar.next()
 
     for name, Model in models.items():
 
@@ -78,10 +82,13 @@ def rate_models (X: pd.DataFrame, Y: pd.DataFrame, verbose=True) -> tuple:
 
         if verbose:
             print(f"\n{name}:\nmean_squared_error: {mse}\nmean_absolute_error: {mae}\nr2_score: {r2}")
+        
+        bar.next()
 
     model = create_model(input_shape=X.shape[1])
     model.fit(x_train, y_train, batch_size=64, epochs=30, verbose=0)
-    joblib.dump(model, f"models/MyModelRegression.pkl")
+    joblib.dump(model, f"models/ModelRegression.pkl")
+    bar.next()
 
     y_pred = model.predict(x_test)
     
@@ -89,10 +96,11 @@ def rate_models (X: pd.DataFrame, Y: pd.DataFrame, verbose=True) -> tuple:
     mae = mean_absolute_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
 
-    names.append("MyModelRegression")
+    names.append("ModelRegression")
     mse_scores.append(mse)
     mae_scores.append(mae)
     r2_scores.append(r2)
+    bar.next()
 
     if verbose:
         print(f"\nMyModelRegression:\nmean_squared_error: {mse}\nmean_absolute_error: {mae}\nr2_score: {r2}")
@@ -108,8 +116,7 @@ def rate_models (X: pd.DataFrame, Y: pd.DataFrame, verbose=True) -> tuple:
 ## \return None
 def create_models_charts (models_rating: tuple, fcharts: str) -> None:
     names, mse_scores, mae_scores, r2_scores = models_rating
-
-    print(names, mse_scores, mae_scores, r2_scores, sep="\n\n")
+    bar = IncrementalBar('Построение графиков...', max=3)
 
     sns.set_style("darkgrid")
     plt.figure(figsize=(20,10))
@@ -118,22 +125,26 @@ def create_models_charts (models_rating: tuple, fcharts: str) -> None:
     plt.xlabel("r2_score")
     plt.ylabel("Названия моделей")
     plt.savefig(f"{fcharts}/r2_scores")
+    bar.next()
     
     sns.barplot(x=mse_scores, y=names)
     plt.xlabel("Mean-Squared-Error")
     plt.ylabel("Названия моделей")
     plt.savefig(f"{fcharts}/MSE")
+    bar.next()
 
     sns.barplot(x=mae_scores, y=names)
     plt.xlabel("Mean-Absolute-Error")
     plt.ylabel("Названия моделей")
     plt.savefig(f"{fcharts}/MAE")
+    bar.next()
 
 ## \brief Функция расчета лучшей модели по трем метрикам
 ## \authors ivan-dev-lab-home
-## \version 1.0.0
+## \version 1.1.0
 ## \date 13.08.2023
-## \param[in] models_rating Рейтинг моделей, полученный из функции rate_models 
+## \param[in] models_rating Рейтинг моделей, полученный из функции rate_models
+## \param[in] fpath Путь до файла с лучшими моделями  
 ## \details Программа ищет минимальную метрику из каждого массива ( mse_scores, mae_scores, r2_scores)
 ## \details Далее в массиве names происходит поиск имени модели по индексу минимальной ( в случае r2_scores - максимальной) метрики в массиве ( mse_scores, mae_scores, r2_scores)
 ## \code
@@ -153,7 +164,7 @@ def create_models_charts (models_rating: tuple, fcharts: str) -> None:
 #     print(f"Метрика {metric} - лучший результат у {result[0]} = {result[1]}")
 ## \endcode
 ## \return Словарь dict() с тремя лучшими моделями по метрикам mse, mae, r2_score
-def get_best_models (models_rating: tuple) -> dict:
+def get_best_models (models_rating: tuple, fpath: str) -> dict:
     best_models = {
         "mse": list,
         "mae": list,
@@ -164,6 +175,13 @@ def get_best_models (models_rating: tuple) -> dict:
     best_models["mse"] = [names[mse_scores.index(min(mse_scores))], min(mse_scores)]
     best_models["mae"] = [names[mae_scores.index(min(mae_scores))], min(mae_scores)]
     best_models["r2_score"] = [names[r2_scores.index(max(r2_scores))], max(r2_scores)]
+    
+    best_models_df = pd.DataFrame(data=best_models)
+
+    if fpath.find(".csv") != -1:
+        best_models_df.to_csv(fpath)
+    elif fpath.find(".xlsx") != -1:
+        best_models_df.to_excel(fpath)
 
     return best_models
 
@@ -178,17 +196,17 @@ def get_best_models (models_rating: tuple) -> dict:
 ## \endcode
 ## \code
 # else:
-#     raise IncorrectParameterError(f'Введенный параметр [top_models_num={top_models_num}] является некорректным т.к является больше, чем количество нейросетей, участвующих в рейтинге')
+#     raise IncorrectParameterError(f'Введенный параметр [num_top={num_top}] является некорректным т.к является больше, чем количество нейросетей, участвующих в рейтинге')
 ## \endcode
 class IncorrectParameterError (Exception): pass
 
 ## \brief Функция возврата n-лучших моделей по метрике
 ## \authors ivan-dev-lab-home
-## \version 1.0.0
+## \version 1.0.1
 ## \date 16.08.2023
 ## \param[in] models_rating Рейтинг моделей, полученный из функции rate_models
 ## \param[in] top_by Метрика, на основании которой будет составлять топ n лучших моделей 
-## \param[in] top_models_num Количество моделей, которые зайдут в топ
+## \param[in] num_top Количество моделей, которые зайдут в топ
 ## \brief Объяснение кода
 ## \details В приведенных ниже конструкциях выполняется добавления названия модели с наилучшим результатом. В конструкции выполняется поиск названия модели в массиве names по индексу наилучшего результата в массиве с соотвествующими метрииками
 ## \code
@@ -220,13 +238,13 @@ class IncorrectParameterError (Exception): pass
 #         os.remove(f"models/{model_file}")
 ## \endcode
 ## \return Список list() с n-лучшими моделями по определенному признаку
-def get_models_top (models_rating: tuple, top_by: str, top_models_num: int=3) -> list:
+def get_models_top (models_rating: tuple, top_by: str, num_top: int=3) -> list[str]:
     models_top = []
     names, mse_scores, mae_scores, r2_scores = models_rating
     
-    if top_models_num <= len(names):
+    if num_top <= len(names):
         if top_by in ["mse", "mae", "r2_score"]:
-            for _ in range(top_models_num):
+            for _ in range(num_top):
                 if top_by == "mse":
                     models_top.append(names[mse_scores.index(min(mse_scores))])
                     mse_scores.pop(mse_scores.index(min(mse_scores)))
@@ -245,6 +263,6 @@ def get_models_top (models_rating: tuple, top_by: str, top_models_num: int=3) ->
         else:
             raise IncorrectParameterError(f'Введенный параметр [top_by={top_by}] является некорректным т.к не принадлежит последовательности ["mse", "mae", "r2_score"])')
     else:
-        raise IncorrectParameterError(f'Введенный параметр [top_models_num={top_models_num}] является некорректным т.к является больше, чем количество нейросетей, участвующих в рейтинге')
+        raise IncorrectParameterError(f'Введенный параметр [num_top={num_top}] является некорректным т.к является больше, чем количество нейросетей, участвующих в рейтинге')
 
     return models_top
