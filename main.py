fc77b007a276a97a6dbc181142fda06323b1a8e9
Ivan <ivanulanovskij143@gmail.com>
2023-08-24 23:33:26 +0500

    исправлены ошибки бета-версии, см. комментарий

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import argparse
 import os
 import re
 import joblib
-from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.ensemble import HistGradientBoostingClassifier
 from preprocess import preprocess
-from create_model import create_model
 
 ## \brief Пользовательское исключение
 ## \authors ivan-dev-lab
@@ -19,23 +19,18 @@ class IncorrectNumberOfArgumentsError (Exception): pass
 
 ## \brief Функция-коммуникатор между пользователем и моделью
 ## \authors ivan-dev-lab
-## \version 1.1.2
-## \date 22.08.2023
+## \version 1.2.2
+## \date 24.08.2023
 ## \details Функция обеспечивает коммуникацию между моделью и пользователем путем создания флагов для комадной строки
-## \brief Подумать над сохранением исходного models_rating
 ## \returns Пространство имен argparse.Namespace
 def make_communication () -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Keeper_AI - Cистема классификации клиентов.")
 
-    data_arg_group = parser.add_argument_group(title="Сохранение и загрузка данных", description="Если не указывать флаг --pred, то данные с предсказаниями будут загружаться в C:/Users/User/Keeper_AI-work/predictions/clients.csv")
-    data_arg_group.add_argument("--clients", type=str, help="Путь до исходных данных в формате [.CSV|.XLSX]") # В КОНЦЕ ОБЯЗАТЕЛЬНО СДЕЛАТЬ required=True
-    data_arg_group.add_argument("--pred", type=str, help="Путь до предполагаемого файла в формате [.CSV|.XLSX] с конечными данными", default="C:/Users/User/Keeper_AI-work/predictions/clients.csv")
-    data_arg_group.add_argument("--save_models", type=str, help="Путь до предполагаемого каталога с сохраненными моделями из составленного топа")
-    
-    rate_arg_group = parser.add_argument_group(title="Оценка различных моделей", description="Если не проводить оценку моделей, то для работы с данными будет использоваться модель по умолчанию. При использовании только флага --rate из данной группы все будет сохранено в каталог по умолчанию в C:/Users/User/Keeper_AI-work/rating/ . Если не использовать свое значение при флаге --num_top, то топ будет включать в себя 3 позиции")
-    rate_arg_group.add_argument("--rate", action="store_true", help="флаг определяет выполнение задачи оценки по умолчанию")
-    rate_arg_group.add_argument("--charts", type=str, help="Путь до предполагаемого каталога с графиками", default="C:/Users/User/Keeper_AI-work/rating/charts/")
-    rate_arg_group.add_argument("--best", type=str, help="Путь до предполагаемого файла в формате [.CSV|.XLSX] с оценкой моделей", default="C:/Users/User/Keeper_AI-work/rating/best.csv")
+    data_arg_group = parser.add_argument_group(title="Сохранение и загрузка данных", description="Если не указывать флаг --pred, то данные с предсказаниями будут загружаться в C:/Users/User/Keeper_AI-work/clients.csv")
+    # В КОНЦЕ ОБЯЗАТЕЛЬНО СДЕЛАТЬ required=True
+    data_arg_group.add_argument("--clients", type=str, help="Путь до исходных данных в формате [.CSV|.XLSX]", required=True) 
+    data_arg_group.add_argument("--pred", type=str, help="Путь до предполагаемого файла в формате [.CSV|.XLSX] с конечными данными", default="C:/Users/User/Keeper_AI-work/clients.csv")
+    # В КОНЦЕ ОБЯЗАТЕЛЬНО СДЕЛАТЬ required=True
 
     train_arg_group = parser.add_argument_group(title="Тренировка моделей", description="При тренировки модели для предсказаний и моделей для оценки, будут использоваться данные по-умолчанию из каталога Keeper_AI/data/train.csv")
     train_arg_group.add_argument("--train", action="store_true", help="Флаг определяет необходимость обучения моделей")
@@ -77,63 +72,72 @@ def create_request () -> dict:
 
 ## \brief Главная функция в которой собраны все остальные функци проекта
 ## \authors ivan-dev-lab
-## \version 1.1.0
-## \date 23.08.2023
+## \version 2.1.0
+## \date 24.08.2023
+## \bug ValueError: The feature names should match those that were passed during fit.<br>Feature names unseen at fit time:<br>- Unnamed: 0 <br> при clients_data["Исход"] = model.predict(clients_data_prep)
 ## \returns None
 def main ():
-    # request временно заполнен на время разработки, в конце должно быть request = create_request ()
+    # request = create_request ()
     request = {
-        'clients': 'C:/Users/User/Desktop/clients.csv',
-        'pred': 'C:/Users/User/Desktop/Keeper_AI-work/clients_preprocessed.xlsx',
-        'save_models': 'C:/Users/User/Desktop/Keeper_AI-work/models/',
-        'rate': True,
-        'charts': 'C:/Users/User/Desktop/Keeper_AI-work/charts',
-        'best': 'C:/Users/User/Desktop/Keeper_AI-work/rating/best.csv',
-        'top': 'C:/Users/User/Desktop/Keeper_AI-work/rating/top.csv',
-        'top_by': 'r2_score',
-        'num_top': 4,
+        'clients': 'C:/Users/User/Keeper_AI-work/clients.csv',
+        'pred': 'C:/Users/User/Keeper_AI-work/clients.csv',
         'train': True
-    }
-    # СДЕЛАТЬ ПРОВЕРКУ НА СУЩЕТВОВАНИЕ У КАТАЛОГОВ ВЕЗДЕ
-        
-    clients_data = preprocess(request["clients"], data_type="test")
-    X,Y = preprocess("data/train.csv", data_type="train")
-    X = X.iloc[0:100000]
-    Y = Y.iloc[0:100000]
+    } 
+
+    DEST_DIR = request["pred"].split("/")
+    DEST_DIR.pop()
+    DEST_DIR = "/".join(DEST_DIR)
     
-    model = create_model(input_shape=X.shape[1])
+    if os.path.exists(DEST_DIR) == False:
+        os.makedirs(DEST_DIR)
 
-    if request['train'] and request['rate'] == False:
-        x_train, x_test, y_train, y_test = train_test_split(X,Y, test_size=0.2, random_state=42)
-        model.fit(x_train, y_train, batch_size=64, epochs=10, verbose=0)
+    clients_data, clients_data_prep = None, None
 
-        joblib.dump(model, "models/ModelRegression.pkl")
-    elif request['train'] == False:
-        model = joblib.load("models/ModelRegression.pkl")
+    if request['clients'].find(".csv") != -1:
+        clients_data = pd.read_csv(request["clients"], index_col=[0])
+        clients_data = clients_data[["CustomerID"]]
+    
+        clients_data_prep = preprocess(request['clients'], data_type="test")
+    elif request['clients'].find(".xlsx") != -1:
+        clients_data = pd.read_excel(request['clients'])
+        clients_data.to_csv(f'{DEST_DIR}/clients.csv')
 
-    if request['rate']:
-        from rate import rate_models, create_models_charts, get_best_models, get_models_top
-        models_rating = rate_models(X,Y, verbose=False)
+        clients_data = pd.read_csv(f'{DEST_DIR}/clients.csv', index_col=[0])
+        clients_data = clients_data[["CustomerID"]]
+    
+        clients_data_prep = preprocess(f'{DEST_DIR}/clients.csv', data_type="test")
 
-        os.makedirs(request['charts'])
-        create_models_charts(models_rating, request['charts'])
+        os.remove(f'{DEST_DIR}/clients.csv')
 
-        get_best_models(models_rating, fpath=request['best'])
+    
+    X,Y = preprocess("data/train.csv", data_type="train")
+    X = X.iloc[0:25000]
+    Y = Y.iloc[0:25000]
+    
+    model = HistGradientBoostingClassifier()
 
-        models_top = get_models_top(models_rating, top_by=request['top_by'], num_top=request["num_top"])
+    if os.path.exists(DEST_DIR+"/model/") == False:
+        os.makedirs(DEST_DIR+"/model/")
 
-        model = joblib.load(f'models/{models_top[0]}.pkl')
+    if request['train']:
+        model.fit(X, Y)
+        joblib.dump(model, f"{DEST_DIR}/model/HistGradientBoostingClassifier.pkl")
+    else:
+        if os.path.exists(f"{DEST_DIR}/model/HistGradientBoostingClassifier.pkl"):
+            model = joblib.load(f"{DEST_DIR}/model/HistGradientBoostingClassifier.pkl")
+        else:
+            model.fit(X, Y)
+            joblib.dump(model, f"{DEST_DIR}/model/HistGradientBoostingClassifier.pkl")
 
-    clients_data["Исход"] = model.predict(clients_data)
+    clients_data["Исход"] = model.predict(clients_data_prep)
+
 
     if request['pred'].find(".csv") != -1:
         clients_data.to_csv(request['pred'])
     elif request['pred'].find(".xlsx") != -1:
         clients_data.to_excel(request['pred'])
-
-
-
-
+    
+    print(f"Анализ данных закончен.\nФайл с предсказаниями системы находится по адресу {request['pred']}")
 
 if __name__ == "__main__":
     main()
